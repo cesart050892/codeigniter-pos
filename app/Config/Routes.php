e6addff992b42f3dec8373dd4f2a6fbe5f1a799a
Cesar Augusto Tapia <cesart050892@gmail.com>
2021-10-01 11:40:05 -0600

    agregando vista de ingreso

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,7 @@ $routes->setAutoRoute(false);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+$routes->get('/', 'Auth/Login::index');
 $routes->get('dashboard', 'Dashboard::index');
 
 /*
