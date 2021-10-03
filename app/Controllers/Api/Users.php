72f38b12e7a596061cc4db08a031bac603a7db9d
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-10-03 13:15:42 -0600

    (api) editando usuario

--- app/Controllers/Api/Users.php
+++ app/Controllers/Api/Users.php
@@ -71,7 +71,7 @@ class Users extends ResourceController
         }
 
         $user->credential_fk = $authModel->insertID();
-        $user->state = 1;
+        $user->state = $this->request->getPost('state') ? $this->request->getPost('state') : 1;
         $user = $user->fill($this->request->getPost(['name', 'rol_fk']));
 
         if (!$this->model->save($user)) {
@@ -109,6 +109,53 @@ class Users extends ResourceController
                 ));
             } else {
                 return $this->failNotFound('can\'t be no found it');
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            return $this->failServerError();
+        }
+    }
+
+    public function update($id = null)
+    {
+        try {
+            //
+            if ($this->validate(array(
+                'name' => 'required',
+            ))) {
+                $user = $this->model->getOne($id);
+                $user->fill($this->request->getPost(['name', 'email', 'rol_fk']));
+                $user->state = $this->request->getPost('state') ? $this->request->getPost('state') : 1;
+                if ($file = $this->request->getFile('image')) {
+                    if ($this->validate([
+                        "image" => 'is_image[image]|max_size[image,1024]' // moidifique el JS para aceptar pdf y testear la validacion
+                    ])) {
+                        if ($file->isValid()) { // el usuario cambio la imagen
+                            if (!$newImage = $user->saveProfileImage($file)) {
+                                return $this->failValidationErrors('Image is no valid!');
+                            }
+                            $user->photo = $newImage;
+                        }
+                    }
+                }
+
+                $authModel = model('App\Models\Credentials', false);
+                $auth = $authModel->find($user->credential_fk);
+                $auth->fill($this->request->getPost(['email', 'username', 'password']));
+
+                if (!$authModel->save($auth)) {
+                    return $this->failValidationErrors($authModel->errors());
+                }
+
+                if (!$this->model->save($user)) {
+                    return $this->failValidationErrors($this->model->errors());
+                }
+
+                return $this->respondUpdated(array(
+                    'message' => 'updated'
+                ));
+            } else {
+                return $this->failValidationErrors($this->validator->getErrors());
             }
         } catch (\Throwable $th) {
             //throw $th;
