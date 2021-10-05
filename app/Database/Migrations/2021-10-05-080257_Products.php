ef9078cb722511fb0120047ff8c7c8814da249a1
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-10-05 02:11:41 -0600

    creando modelo de producto

---
+++ app/Database/Migrations/2021-10-05-080257_Products.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Products extends Migration
+{
+    protected $name = 'products';
+
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'BIGINT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'code'    => [
+                'type'       => 'TEXT'
+            ],
+            'description'    => [
+                'type'       => 'TEXT'
+            ],
+            'stock'    => [
+                'type'           => 'BIGINT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'cost'    => [
+                'type'           => 'DOUBLE',
+                'constraint'     => '11,2',
+            ],
+            'sale'    => [
+                'type'           => 'DOUBLE',
+                'constraint'     => '11,2',
+            ],
+            'quantity'    => [
+                'type'           => 'BIGINT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'image'    => [
+                'type'       => 'VARCHAR',
+                'constraint' => '75',
+                'null'             => false
+            ],
+            'category_fk'    => [
+                'type'           => 'BIGINT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addField("created_at DATETIME NULL DEFAULT NULL");
+        $this->forge->addField("updated_at DATETIME NULL DEFAULT NULL");
+        $this->forge->addField("deleted_at DATETIME NULL DEFAULT NULL");
+        $this->forge->addForeignKey('category_fk', 'categories', 'id', 'cascade', 'cascade');
+        $this->forge->createTable($this->name);
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable($this->name);
+    }
+}
